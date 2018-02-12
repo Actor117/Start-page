43595928fe53e70eba6dea9739fc0618ff0d482b
Ali Almoullim <ali.almoullim@gmail.com>
2018-02-12 07:12:13 +0300

    setup for color themes

---
+++ colors.js
@@ -0,0 +1,11 @@
+$(function () {
+    chrome.storage.sync.get(
+        {
+            'red': '[b30000, 202D32]',
+            'blue': '[1e6f6f, 202D32]',
+            'green': '[007515, 203222]',
+            'brown': '[6b420e, 322d20]'
+        }
+    )
+
+})
