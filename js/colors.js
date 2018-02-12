b29ad54eb7ff1e94ff5629b92188d6fce1c0e5c9
Ali Almoullim <ali.almoullim@gmail.com>
2018-02-12 11:42:33 +0300

    Configure saving the chosen color

--- js/colors.js
+++ js/colors.js
@@ -1,15 +1,25 @@
 $(function () {
-    // chrome.storage.sync.get(
-var c = {
+    chrome.storage.sync.get(
+        {
             'red': ['b30000', '202D32'],
             'blue': ['1e6f6f', '202D32'],
             'green': ['007515', '203222'],
-            'brown': ['6b420e', '322d20']
-        }
-//         }, colorCallBack)
-//     });
-//
-// var colorCallBack = function (c) {
+            'brown': ['6b420e', '322d20'],
+            'current': 'blue'
+        }, colorCallBack)
+    });
+
+var colorCallBack = function (c) {
+
+    var color = c['current'];
+    $('.time').css('color', '#'+c[color][0]);
+    $('input').css('background-color', '#'+c[color][0]);
+    $('.site-box').css('background-color', '#'+c[color][0]);
+    $('.white').css('background-color', '#'+c[color][1]);
+    $('body').css('background-color', '#'+c[color][1]);
+    $('body').toggle('fade', 400);
+    $('input').focus();
+
 
         $('.white').on('click', function() {
             $(this).toggle('.hide')
@@ -25,6 +35,8 @@ var c = {
                 $('.white').animate({'background-color': '#'+c[name][1]},200);
                 $('.white').toggle('.hide')
                 $('span').toggle('.hide')
+                chrome.storage.sync.set({'current': name})
+                console.log(c['current']);
             });
         }
 
@@ -32,4 +44,4 @@ var c = {
         recoler('blue');
         recoler('green');
         recoler('brown');
-});
+    }
