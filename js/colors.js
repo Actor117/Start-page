e33577f4977339bcc33a78d5aa4b126851bbe659
Ali Almoullim <ali.almoullim@gmail.com>
2018-02-12 14:31:47 +0300

    fix erros

--- js/colors.js
+++ js/colors.js
@@ -1,35 +1,45 @@
-$(function () {
+$(function() {
     var c = {
-            'red': ['b30000', '202D32'],
-            'blue': ['1e6f6f', '202D32'],
-            'green': ['007515', '203222'],
-            'brown': ['6b420e', '322d20'],
-            'current': 'blue',
-        };
+        'red': ['b30000', '202D32'],
+        'blue': ['1e6f6f', '202D32'],
+        'green': ['007515', '203222'],
+        'brown': ['6b420e', '322d20'],
+        'current': 'blue',
+    };
 
-        $('body').toggle('fade')
+    $('body').toggle('fade');
 
-        $('.white').on('click', function() {
-            $(this).toggle('.hide')
-            $('span').toggle('.hide')
-        });
-
-        function recoler(name) {
-            $('.'+name+'').on('click', function() {
-                $('.time').animate({'color': '#'+c[name][0]},200);
-                $('input').animate({'background-color': '#'+c[name][0]},200);
-                $('.site-box').animate({'background-color': '#'+c[name][0]},200);
-                $('body').animate({'background-color': '#'+c[name][1]},200);
-                $('.white').animate({'background-color': '#'+c[name][1]},200);
-                $('.white').toggle('.hide')
-                $('span').toggle('.hide')
-//                 chrome.storage.sync.set({'current': name})
-//                 console.log(c['current']);
-            });
-        }
-
-        recoler('red');
-        recoler('blue');
-        recoler('green');
-        recoler('brown');
+    $('.white').on('click', function() {
+        $(this).toggle('.hide')
+        $('span').toggle('.hide')
     });
+
+    function recoler(name) {
+        $('.' + name + '').on('click', function() {
+            $('.time').animate({
+                'color': '#' + c[name][0]
+            }, 200);
+            $('input').animate({
+                'background-color': '#' + c[name][0]
+            }, 200);
+            $('.site-box').animate({
+                'background-color': '#' + c[name][0]
+            }, 200);
+            $('body').animate({
+                'background-color': '#' + c[name][1]
+            }, 200);
+            $('.white').animate({
+                'background-color': '#' + c[name][1]
+            }, 200);
+            $('.white').toggle('.hide')
+            $('span').toggle('.hide')
+            //                 chrome.storage.sync.set({'current': name})
+            //                 console.log(c['current']);
+        });
+    }
+
+    recoler('red');
+    recoler('blue');
+    recoler('green');
+    recoler('brown');
+});
